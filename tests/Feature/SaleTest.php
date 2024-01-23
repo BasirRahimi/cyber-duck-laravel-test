c5783048afe8c96edb01980cc03c64c2feb922a1
Basir Rahimi <basiralirahimi@gmail.com>
2024-01-23 22:40:51 +0000

    inital sales test scaffolding

--- tests/Feature/SaleTest.php
+++ tests/Feature/SaleTest.php
@@ -17,4 +17,26 @@ class SaleTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    /**
+     * Test the selling price calculation returns the correct value.
+     */
+
+    public function test_selling_price_calculation(): void
+    {
+    }
+
+    /**
+     * Test creating a sale with valid data.
+     */
+    public function test_creating_a_sale_with_valid_data(): void
+    {
+    }
+
+    /**
+     * Test creating a sale with invalid data.
+     */
+    public function test_creating_a_sale_with_invalid_data(): void
+    {
+    }
 }
